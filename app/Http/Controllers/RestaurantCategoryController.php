cc91577dac10b0ff5b95067b85804c7b5d629d74
adremis <adremis.ak@gmail.com>
2022-06-29 00:36:23 +0430

    crud controller restaurant category

---
+++ app/Http/Controllers/RestaurantCategoryController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\RestaurantCategory;
+use App\Http\Requests\StoreRestaurantCategoryRequest;
+use App\Http\Requests\UpdateRestaurantCategoryRequest;
+
+class RestaurantCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $resCategories=RestaurantCategory::paginate(5);
+        return view('category.restaurant.index',compact('resCategories'));
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreRestaurantCategoryRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreRestaurantCategoryRequest $request)
+    {
+        RestaurantCategory::create($request->all());
+        return redirect()->route('restaurantCategories.index');
+    }
+
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\RestaurantCategory  $restaurantCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(RestaurantCategory $restaurantCategory)
+    {
+        return view('category.restaurant.edit',compact('restaurantCategory'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateRestaurantCategoryRequest  $request
+     * @param  \App\Models\RestaurantCategory  $restaurantCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateRestaurantCategoryRequest $request, RestaurantCategory $restaurantCategory)
+    {
+        $restaurantCategory->update($request->all());
+        return redirect()->route('restaurantCategories.index');
+    }
+
+
+    public function destroy(RestaurantCategory $restaurantCategory)
+    {
+        $restaurantCategory->delete();
+        return redirect()->route('restaurantCategories.index');
+    }
+}
