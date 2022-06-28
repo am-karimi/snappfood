332e6ab333ee3e23cc5aa73202219bf03e931fa3
adremis <adremis.ak@gmail.com>
2022-06-29 01:50:42 +0430

    create migration to food categories

---
+++ database/migrations/2022_06_28_205434_create_food_categories_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Models\RestaurantCategory;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('food_categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug');
+
+            $table->foreignId('restaurant_category_id');
+            $table->foreign('restaurant_category_id')
+                ->references('id')
+                ->on('restaurant_categories')
+                ->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('food_categories');
+    }
+};
