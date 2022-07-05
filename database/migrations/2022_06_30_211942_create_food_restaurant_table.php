adbb1d0ef980c42e4665498669f1248ced2a4d74
adremis <adremis.ak@gmail.com>
2022-07-06 00:58:02 +0430

    update food restaurant pivot migration

--- database/migrations/2022_06_30_211942_create_food_restaurant_table.php
+++ database/migrations/2022_06_30_211942_create_food_restaurant_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('food_restaurants', function (Blueprint $table) {
+        Schema::create('food_restaurant', function (Blueprint $table) {
 
             //foreign key 1
             $table->foreignId('restaurant_id');
