3065806de22c8669748c3cb56c0e798377832536
adremis <adremis.ak@gmail.com>
2022-07-06 00:58:45 +0430

    app view update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
+    <style>
+        @yield('background')
+    </style>
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <script
