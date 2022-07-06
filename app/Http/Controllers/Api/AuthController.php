4b8048d7497059b809fa05316222297db96bdfa4
adremis <adremis.ak@gmail.com>
2022-07-06 15:44:38 +0430

    add Api Auth Controller

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,13 +5,31 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
     public function login(Request $request)
     {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+        $user = User::where('email', $request->email)->first();
 
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['User Name or Password InCorrect.'],
+            ]);
+        }
+        $token=$user->createToken('userToken')->plainTextToken;
+        return [
+            'token' => $token,
+            'message'=>'Login Successfull'
+        ];
     }
+
 
     public function register(Request $request)
     {
@@ -32,7 +50,7 @@ class AuthController extends Controller
         $token=$user->createToken('userToken')->plainTextToken;
         return [
             'token' => $token,
-            'message'=>'Register Successfull'
+            'message'=>'Register Successfully'
         ];
     }
 }
