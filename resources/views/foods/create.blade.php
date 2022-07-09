45c5aabd24523f6eb636f672f31d139d8dfdd5ec
adremis <adremis.ak@gmail.com>
2022-07-09 22:36:04 +0430

    update food view

--- resources/views/foods/create.blade.php
+++ resources/views/foods/create.blade.php
@@ -37,8 +37,8 @@
                 dark:text-gray-300">
                     Food Category Id
                 </label>
-                <select id="food_category_id" name="food_category_id[]"
-                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 h-16" multiple>
+                <select id="food_category_id" name="food_category_id"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 " >
                     @foreach($foodCategories as $foodCategory)
                         <option value="{{$foodCategory->id}}">{{$foodCategory->name}}</option>
                     @endforeach
