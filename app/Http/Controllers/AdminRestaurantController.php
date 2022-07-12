8429efec78b5324cc561e7ea722643818d995fb2
adremis <adremis.ak@gmail.com>
2022-07-13 02:01:44 +0430

    change admin restaurant controller

--- app/Http/Controllers/AdminRestaurantController.php
+++ app/Http/Controllers/AdminRestaurantController.php
@@ -3,14 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Models\Restaurant;
-use Illuminate\Http\Request;
 
 class AdminRestaurantController extends Controller
 {
     public function index()
     {
         $restaurants=Restaurant::with('user','address','restaurantCategories')->paginate(5);
-        return view('restaurants.admin.index',compact($restaurants));
+        return view('restaurants.admin.index',compact('restaurants'));
+    }
+
+    public function show(Restaurant $restaurant)
+    {
+        $owner=$restaurant->user;
+        $address=$restaurant->address;
+        return view('restaurants.admin.showOwner',compact('owner'));
     }
 
 
