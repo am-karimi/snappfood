a902bec99584e7752e13fd5181029f136088e991
adremis <adremis.ak@gmail.com>
2022-07-13 02:06:40 +0430

    update food controller for api resource

--- app/Http/Controllers/Api/FoodController.php
+++ app/Http/Controllers/Api/FoodController.php
@@ -3,20 +3,23 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\Food;
-use Illuminate\Http\Request;
+use App\Http\Resources\FoodCategoriesResource;
+use App\Models\FoodCategory;
 
 class FoodController extends Controller
 {
     public function getFoods($restaurant_id)
     {
-        $foods=Food::whereHas('restaurants',function ($query) use ($restaurant_id){
-            $query->where('restaurant_id',$restaurant_id);
-        })->get();
-        return response()->json(
-            [
-                'Foods' => $foods,
-            ]
-        );
+        /*
+                  get food with restaurant - worst way
+        //        $foods=Food::whereHas('restaurants',function ($query) use ($restaurant_id){
+        //            $query->where('restaurant_id',$restaurant_id);
+        //        })->get();
+        */
+
+#       get categories with foods - good way
+        $categories=FoodCategory::with('foods')->get();
+#       send data for api by api resource
+        return response()->json(FoodCategoriesResource::collection($categories));
     }
 }
