6d99be79e422f16a257a20778ff0e8dd27e37c9c
adremis <adremis.ak@gmail.com>
2022-07-15 13:42:34 +0430

    create discounts table

---
+++ database/migrations/2022_07_13_203902_add_discount_id_to_food_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Models\Discount;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('food', function (Blueprint $table) {
+            $table->foreignIdFor(Discount::class)->after('food_category_id');
+//            $table->foreign('discount_id')
+//                ->references('id')->on('discounts')->onDelete('cascade');
+//
+//            $table->foreignId('discount_id');
+//            $table->foreign('discount_id')
+//                ->references('id')
+//                ->on('discounts')
+//                ->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('food', function (Blueprint $table) {
+            $table->dropColumn('discount_id');
+        });
+    }
+};
