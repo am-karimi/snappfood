09f27bf478e11bcaa8bd66656cdb424b959cba9e
adremis <adremis.ak@gmail.com>
2022-07-26 10:51:56 +0430

    update route file

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,7 @@
 <?php
 
-use App\Http\Controllers\AdminRestaurantController;
-use App\Http\Controllers\DiscountController;
-use App\Http\Controllers\FoodCategoryController;
-use App\Http\Controllers\FoodController;
-use App\Http\Controllers\RestaurantCategoryController;
-use App\Http\Controllers\RestaurantController;
-use App\Http\Controllers\SellerRestaurantController;
-use App\Http\Controllers\UserController;
+
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,6 +19,8 @@ Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
+
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
@@ -33,29 +29,13 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('layouts/dashboard');
     })->name('dashboard');
-
-
-    Route::post('/users/status', [UserController::class, 'status'])->name('status');
-    Route::resource('/users', UserController::class);
-
-    Route::get('/discounts/chooseRestaurant',[DiscountController::class, 'chooseRestaurant'])
-        ->name('discounts.chooseRestaurant');
-    Route::post('/discounts/create',[DiscountController::class,'create'])
-        ->name('discounts.create');
-    Route::resource('/discounts', DiscountController::class)
-        ->except('create','update','show','edit');
-
-    Route::resource('category/restaurantCategories', RestaurantCategoryController::class);
-    Route::resource('category/foodCategories', FoodCategoryController::class);
-
-    Route::post('foods/restaurantFilter', [FoodController::class,'restaurantFilter'])->name('foods.restaurantFilter');
-    Route::post('foods/categoryFilter', [FoodController::class,'categoryFilter'])->name('foods.categoryFilter');
-
-
-//    Route::as('admin')->resource('admin/foods', AdminFoodController::class);
-    Route::resource('foods', FoodController::class);
-    Route::as('admin')->resource('admin/restaurants', AdminRestaurantController::class);
-    Route::as('seller')->resource('seller/restaurants', SellerRestaurantController::class);
 });
 
-
+require __DIR__ . '/web/category.php';
+require __DIR__ . '/web/discount.php';
+require __DIR__ . '/web/seller.php';
+require __DIR__ . '/web/admin.php';
+require __DIR__ . '/web/food.php';
+require __DIR__ . '/web/user.php';
+require __DIR__ . '/web/workingHours.php';
+require __DIR__ . '/web/order.php';
